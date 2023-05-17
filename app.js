ee045b54aa0133ca2a390999df554042c0795c32
dimitri1989 <dimitri.kvarelashvili@gmail.com>
2023-05-17 15:35:51 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function loading(){
+    var load = document.getElementById("loadings").style.display="none";
+    var load2 = document.getElementById("loading2").style.display="flex";
+}
+function cancel(){
+    var load = document.getElementById("loadings").style.display="flex";
+    var load2 = document.getElementById("loading2").style.display="none";
+}
